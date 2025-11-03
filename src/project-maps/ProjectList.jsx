bd3da12f8f8726fbef260288bf8a86a26d841551
MichaelSuthirta <michaelsuthirta@gmail.com>
2025-11-03 22:46:04 +0700

    Feat: Created project map list

---
+++ src/project-maps/ProjectList.jsx
@@ -0,0 +1,194 @@
+export const projects = [
+    {
+        title: "SpeechTeach",
+        mainImgUrl: "src\\assets\\images\\SpeechTeach-logo.png",
+        date: "2025",
+        description: "An application utilizing fine-tuned Whisper model to help English learners in practicing pronunciation. The dictionary was taken from https://github.com/benjihillard/English-Dictionary-Database.",
+        status: "Finished",
+        imagesList: [
+            "src\\assets\\images\\project-screenshoots\\SpeechTeach\\ST1.jpg",
+            "src\\assets\\images\\project-screenshoots\\SpeechTeach\\ST2.jpg",
+            "src\\assets\\images\\project-screenshoots\\SpeechTeach\\ST3.jpg",
+            "src\\assets\\images\\project-screenshoots\\SpeechTeach\\ST4.jpg",
+            "src\\assets\\images\\project-screenshoots\\SpeechTeach\\ST5.jpg"
+        ],
+        role: [
+            "UI/UX Designer",
+            "Fullstack Developer"
+        ],
+        lessons: [
+            "Flutter concepts, such as ChangeNotifier, Future, JSON passing, states, etc.",
+            "Python and Flask backend",
+            "Problem solving",
+            "Critical thinking",
+            "Creativity"
+        ],
+        githubLink: "https://github.com/MichaelSuthirta/speech-teach",
+        demoLink: "https://drive.google.com/file/d/1IS9sr2nwLh3uYj9hQeBegBu3cgbx4dj4/view?usp=drive_link",
+        downloadLink: "https://drive.google.com/file/d/1--9i5Q4r9APe4cmK2fJNR8Z4k41kp8q-/view?usp=sharing"
+    },
+
+    {
+        title: "Pathforging Realms",
+        mainImgUrl: "src\\assets\\images\\Pathforging-Realms-img.png",
+        date: "2024-2025",
+        description: "A 2D platformer game using Unity Engine, done in a team.",
+        status: "Unfinished",
+        imagesList:[
+            "src\\assets\\images\\project-screenshoots\\Pathforging-Realms\\Screenshot 2025-09-11 121037.png",
+            "src\\assets\\images\\project-screenshoots\\Pathforging-Realms\\Screenshot 2025-09-11 121822.png",
+            "src\\assets\\images\\project-screenshoots\\Pathforging-Realms\\Screenshot 2025-09-11 121231.png",
+            "src\\assets\\images\\project-screenshoots\\Pathforging-Realms\\Screenshot 2025-09-11 121901.png"
+        ],
+        role: [
+            "Project Leader",
+            "Programmer",
+            "Artist"
+        ],
+        lessons: [
+            "Roles and components in game development",
+            "C# programming",
+            "Unity Engine",
+            "Problem solving",
+            "Creativity"
+        ],
+        githubLink: "https://github.com/MichaelSuthirta/Pathforging-Realms",
+        demoLink: "https://drive.google.com/file/d/1_GjY2KREATVmWiWftxWLaiCffXxnL_5V/view?usp=drive_link"
+    },
+
+    {
+        title: "Fine-tuned Whisper",
+        mainImgUrl: "src\\assets\\images\\whisper-finetune-img.png",
+        date: "2025",
+        description: "A project to fine-tune a Whisper model using Common Native dataset to be used for English voice transcription.",
+        status: "Finished",
+        imagesList: [
+            "src\\assets\\images\\project-screenshoots\\Whisper-Finetune\\Screenshot 2025-09-11 162256.png"
+        ],
+        role: [
+            "Developer"
+        ],
+        lessons: [
+            "AI training methods",
+            "Patience",
+            "Adaptiveness",
+            "Willingness to learn",
+        ],
+        githubLink: "https://github.com/MichaelSuthirta/Finetune-Whisper-Common-Native",
+    },
+
+    {
+        title: "Social Media Virtual Assistant",
+        mainImgUrl: "src\\assets\\images\\virtual-assistant-img.png",
+        date: "2025",
+        description: "A group project that utilizes Vosk and Flutter to create a voice assistant capable of helping elderly with using social media such as Whatsapp.",
+        status: "Unfinished",
+        imagesList: [
+            
+        ],
+        role: [
+            "Frontend developer"
+        ],
+        lessons: [
+            "Flutter states and recording system",
+            "Problem solving",
+        ],
+        githubLink: "https://github.com/MichaelSuthirta/SocMedVirtualAssistant"
+    },
+
+    {
+        title: "Social Media Virtual Assistant",
+        mainImgUrl: "src\\assets\\images\\virtual-assistant-img.png",
+        date: "2025",
+        description: "A group project that utilizes Vosk and Flutter to create a voice assistant capable of helping elderly with using social media such as Whatsapp.",
+        status: "Unfinished",
+        imagesList: [
+            "src\\assets\\images\\project-screenshoots\\Virtual-Assistant\\Screenshot 2025-11-03 221914.png"
+        ],
+        role: [
+            "Frontend developer"
+        ],
+        lessons: [
+            "Flutter states and recording system",
+            "Problem solving",
+        ],
+        githubLink: "https://github.com/MichaelSuthirta/SocMedVirtualAssistant"
+    },
+
+    {
+        title: "Signature Forgery Detection",
+        mainImgUrl: "src\\assets\\images\\signature-detection-img.png",
+        date: "2025",
+        description: "A group project that uses Pytorch to compare YOLOv11, VGG-19, EGACNN, CNN-AE, and Siamese Networks in detecting false signatures.",
+        status: "Finished",
+        imagesList: [
+            "src\\assets\\images\\project-screenshoots\\Signature-Forg-Detection\\Screenshot 2025-11-03 214644.png",
+            "src\\assets\\images\\project-screenshoots\\Signature-Forg-Detection\\Screenshot 2025-11-03 214712.png",
+            "src\\assets\\images\\project-screenshoots\\Signature-Forg-Detection\\Screenshot 2025-11-03 214728.png"
+        ],
+        
+        role: [
+            "Project leader",
+            "Developer",
+            "Worked on VGG19, Siamese Networks  and CNN-AE models"
+        ],
+        lessons: [
+            "Machine learning and AI",
+            "Teamwork",
+            "Leadership",
+            "Problem solving"
+        ],
+        githubLink: "https://github.com/MichaelSuthirta/Signature-Forgery-Detection-Comparison"
+    },
+
+    {
+        title: "Flexinote",
+        mainImgUrl: "src\\assets\\images\\flexinote-img.png",
+        date: "2024",
+        description: "A group project that uses Flutter to create an application to take notes and find e-books. For now, this project still has no database integration, utilizing local storage temporarily.",
+        status: "Uninished",
+        imagesList: [
+            "src\\assets\\images\\project-screenshoots\\Flexinote\\Screenshot 2025-11-03 222018.png",
+            "src\\assets\\images\\project-screenshoots\\Flexinote\\Screenshot 2025-11-03 222038.png",
+            "src\\assets\\images\\project-screenshoots\\Flexinote\\Screenshot 2025-11-03 222052.png"
+        ],
+        role: [
+            "Project manager",
+            "Frontend developer",
+            "Mainly worked on ebook feature"
+        ],
+        lessons: [
+            "Flutter framework and plugins",
+            "Teamwork",
+            "Problem solving"
+        ],
+        githubLink: "https://github.com/MichaelSuthirta/Flexinote",
+        demoLink: "https://drive.google.com/file/d/1pmhbzOH3xSlK8nEzE_WjyfcDISbD58hH/view?usp=drive_link"
+    },
+
+    {
+        title: "The Gold Mine",
+        mainImgUrl: "src\\assets\\images\\gold-mine-img.png",
+        date: "2023",
+        description: "A simple group project that utilizes C language to create a simple Command Prompt game. Its mechanics and score saving system is still very simple, utilizing file operations.",
+        status: "Finished",
+        imagesList: [
+            "src\\assets\\images\\project-screenshoots\\Gold-Mine\\Screenshot 2025-09-23 150343.png",
+            "src\\assets\\images\\project-screenshoots\\Gold-Mine\\Screenshot 2025-09-23 150400.png",
+            "src\\assets\\images\\project-screenshoots\\Gold-Mine\\Screenshot 2025-09-23 150451.png",
+            "src\\assets\\images\\project-screenshoots\\Gold-Mine\\Screenshot 2025-09-23 150608.png"
+        ],
+        
+        role: [
+            "Project leader",
+            "Developer"
+        ],
+        lessons: [
+            "Basic C programming",
+            "Teamwork",
+            "Creativity"
+        ],
+        githubLink: "https://github.com/MichaelSuthirta/Lab-Project-Semester-1",
+        demoLink: "https://drive.google.com/file/d/15qy9dEaBnkM_ZvPQSAxTsBTOu59LKyl_/view?usp=sharing"
+    },
+]
